60a25bf9a754b570b12bae0f69cf7b4b8a833ba0
AshutoshKumar99 <ashutoshkumar101094@gmail.com>
2017-06-20 15:05:03 +0530

    Add files via upload

---
+++ sp.java
@@ -0,0 +1,14 @@
+class sp
+{
+public static void main(String args[])
+{
+for(int i=1;i<=10;i++)
+System.out.println("i is="+i);
+
+
+
+
+
+
+}
+}
---
+++ sp2.java
@@ -0,0 +1,15 @@
+class sp2
+{
+public static void main(String args[])
+{
+
+System.out.println("This is QA2 Side\tThank for using Jenkins.");
+System.out.println("My Laptop name is Dell Vostro 2510,and I have been using it since last 5 years,ThankYou.!");
+System.out.println("Above statement is the statement after commit changes in the file sp2.java");
+
+
+
+
+
+}
+}
